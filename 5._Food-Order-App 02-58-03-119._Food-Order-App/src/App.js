dbff32d4a4c712ae16349c901b4da4495200c06a
Elisha2605 <elisha_ngoma@yahoo.fr>
2022-08-23 01:12:02 +0200

    added .env

--- 5._Food-Order-App 02-58-03-119._Food-Order-App/src/App.js
+++ 5._Food-Order-App 02-58-03-119._Food-Order-App/src/App.js
@@ -6,6 +6,28 @@ import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
 
 function App() {
+
+  const [movie, setMovie] = useState([])
+
+  function fetchData() {
+    fetch("https://swapi.dev/api/films")
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) =>  {
+      const newData = data.results.map(el => {
+        return {
+          title: el.title,
+          episode: el.episode_id,
+          director: el.director
+        }
+      })
+      setMovie(newData);
+    })
+  }
+  fetchData();
+  console.log(movie);
+
   const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
